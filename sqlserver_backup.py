63a0a0780bd27e8649995fb8e8e4fe5bd85b1e46
KLA <smtwkla@gmail.com>
2024-08-28 00:52:58 +0530

    fix do not pause after last running.

--- sqlserver_backup.py
+++ sqlserver_backup.py
@@ -7,15 +7,20 @@ conn = pymssql.connect(server=SERVER_HOST, user=USERNAME, password=PASSWORD)
 conn.autocommit(True)
 cursor = conn.cursor()
 
+is_first = True
 
 for db in DATABASES:
+    if not is_first:
+        time.sleep(PAUSE)
+    is_first = False
+
     file_names = ""
     for i in range(1,FILE_PARTS+1):
         file_names += f"URL='{STORE_URL}{db}-part{i}.bak'" + (", " if i!=FILE_PARTS else "")
     sql = f"BACKUP DATABASE {db} TO {file_names} WITH FORMAT, MAXTRANSFERSIZE=20971520;"
     print(f"Executing : {sql} ...")
     cursor.execute(sql)
-    time.sleep(PAUSE)
+
 
 
 conn.close()
